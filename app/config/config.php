d0781c7a97613dee2e8bb9e45874972ae6769e08
zaw lin phyo <ko148596@gmail.com>
2020-12-12 18:57:01 +0630

    Database

---
+++ app/config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+// Database connection info
+define ('DB_HOST' , 'localhost');
+define ('DB_USER' , 'root');
+define ('DB_PASS' , '');
+define ('DB_NAME' , 'mediazone');
+define ('DB_CHARSET' , 'utf8');
+// App root
+define ('APPROOT' , dirname(dirname(__FILE__)));
+
+// Urlroot
+define ('URLROOT' , 'http://localhost/mediaLibrary');
+
+// site name
+define ('SITENAME' , 'Media Zone');
+
